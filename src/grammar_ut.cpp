a7e75880bf2028b2fa983601e2d48087762eac87
Paul Jarrett <jarrett.paul.young@gmail.com>
2016-08-09 22:33:55 -0400

    Adds multiple production test.

--- src/grammar_ut.cpp
+++ src/grammar_ut.cpp
@@ -12,6 +12,12 @@ TEST(Grammar_Test, Add_Empty_Production) {
   EXPECT_EQ("empty", as_string(grammar["A"_sym]));
 }
 
+TEST(Grammar_Test, Add_Multiple_Alternative_Production) {
+  Grammar grammar;
+  grammar["A"_sym] = {"B"_sym + "C"_sym | "D"_sym | "E"_sym + "F"_sym};
+  EXPECT_EQ("B C | D | E F", as_string(grammar["A"_sym]));
+}
+
 int main(int argc, char ** argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
