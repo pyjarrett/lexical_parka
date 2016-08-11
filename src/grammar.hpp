0c2058abf6f890c216998bbb633bcecc83e3bb12
Paul Jarrett <jarrett.paul.young@gmail.com>
2016-08-10 21:38:14 -0400

    Removes unused helper method declaration.

--- src/grammar.hpp
+++ src/grammar.hpp
@@ -14,7 +14,6 @@ public:
     return productions[sym];
   }
 
-  bool has_empty_production_direct(Symbol const & sym) const;
   bool has_empty_production(Symbol const & sym) const;
   bool is_empty_production(Symbol_String const & symbol_string) const;
   Symbol_Set empty_producing_symbols() const;
